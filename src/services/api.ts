1f81e116ea6839649b07f44dafce24a4a8945d78
Marcello Carvalhal <marcellocontact97@gmail.com>
2022-05-11 11:12:43 -0300

    feat: heroku api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export default axios.create({
   headers: {
     'Content-Type': 'application/json; charset=utf-8',
     'access-control-allow-origin': '*'
   },
 
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT}`
+  baseURL: apiUrl || `http://localhost:${process.env.REACT_APP_API_PORT}`
 });
